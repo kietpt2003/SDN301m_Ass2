c99fabe31abfc12489af283e1caca46c12e332b2
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-21 12:52:38 +0700

    add new post route

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getCategories } from '../controller/apiController'
+import { getCategories, postCategory } from '../controller/categoriesController'
 
 const router = express.Router();
 
 const initCategoryRouter = (app) => {
     router.get('/', getCategories); //method get <=> read data
+    router.post('/', postCategory)
 
     return app.use('/categories', router);
 }
