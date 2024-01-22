ac3051019f19d1e8c1eec0ef60edf92cd3fe7dd2
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-22 20:09:38 +0700

    create delete service

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -27,34 +27,53 @@ export const createCategory = async (newCategory) => {
         try {
             let error = {}
             let isError = false
-            let isExist = await checkCateName(newCategory.cateName);
-            if (isExist) {
-                error.isDup = 'Name Duplicated'
-                resolve(error)
-            }
-            if (newCategory.cateName === '') {
+            let arr = await getAllCategories();
+            if (newCategory.cateName === '' || newCategory.cateName === undefined) {
                 error.isEmptyName = 'Name cannot be empty';
                 isError = true;
             }
-            if (newCategory.description === '') {
+            if (newCategory.description === '' || newCategory.description === undefined) {
                 error.isEmptyDes = 'Description cannot be empty';
                 isError = true;
             }
             if (isError) {
-                resolve(error)
+                resolve({
+                    error: error,
+                    arrCategories: arr
+                })
             }
-            const url = process.env.URL_DB;
-            const connect = mongoose.connect(url, { family: 4 });
-            connect.then(() => {
-                Categories({ name: newCategory.cateName, description: newCategory.description }).save()
-                    .then((cate) => {
-                        if (cate) {
-                            resolve(getAllCategories());
-                        } else {
-                            error.createfailed = 'Something wrong'
-                        }
-                    });
-            })
+            let isExist = await checkCateName(newCategory.cateName);
+            if (isExist) {
+                console.log('dup name');
+                error.isDup = 'Name Duplicated';
+                isError = true;
+                resolve({
+                    error: error,
+                    arrCategories: arr
+                })
+            }
+            if (!isError) {
+                const url = process.env.URL_DB;
+                const connect = mongoose.connect(url, { family: 4 });
+                connect.then(() => {
+                    Categories({ name: newCategory.cateName, description: newCategory.description }).save()
+                        .then(async (cate) => {
+                            if (cate) {
+                                arr = await getAllCategories();
+                                resolve({
+                                    arrCategories: arr,
+                                    isSuccess: true
+                                });
+                            } else {
+                                error.createfailed = 'Something wrong';
+                                resolve({
+                                    error: error,
+                                    arrCategories: arr
+                                })
+                            }
+                        });
+                })
+            }
         } catch (error) {
             reject(error)
         }
@@ -64,6 +83,7 @@ export const createCategory = async (newCategory) => {
 let checkCateName = (name) => {
     return new Promise(async (resolve, reject) => {
         try {
+            console.log('hahah ', name);
             let category = await Categories.findOne({ name: name })
                 .then((category) => {
                     console.log('check: ', category)
