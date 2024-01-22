e5479c8a00452f74fec8db08e3b5c2480fd097b7
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-22 20:09:19 +0700

    create delete cate controller

--- src/controller/categoriesController.js
+++ src/controller/categoriesController.js
@@ -6,7 +6,17 @@ export const getCategories = async (req, res) => {
 }
 
 export const postCategory = async (req, res) => {
-    const arrCategories = await createCategory(req.body);
+    const data = await createCategory(req.body);
+    console.log('check data: ', data);
+    if (data.error) {
+        return res.status(400).render('CategoryPage.ejs', { arrCategories: data.arrCategories, error: data.error });
+    }
     // return res.send(arrCategories);
-    return res.render('CategoryPage.ejs', { arrCategories });
+    return res.render('CategoryPage.ejs', { arrCategories: data.arrCategories, isSuccess: data.isSuccess });
+}
+
+export const deleteCategory = async (req, res) => {
+    console.log('check data: ', req.params.id);
+    // return res.send(arrCategories);
+    return res.render('CategoryPage.ejs', { arrCategories: [] });
 }
