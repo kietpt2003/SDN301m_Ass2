7104aa3e23dae296e9aa223fe068c1a548b59e2d
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-22 20:08:16 +0700

    comment some code for create sample data

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ import { configViewEngine } from './config/viewEngine';
 import { initCategoryRouter } from './routes/categoryRouter';
 import { iniOrchidRoute } from './routes/orchidRouter';
 import { configBodyParse } from './config/configBodyParser';
+
+// import mongoose from 'mongoose';
+// import Categories from './models/Categories';
 const app = express();
 
 //Config Body-Parser
@@ -29,6 +32,65 @@ initCategoryRouter(app);
 
 //Init orchid routes
 iniOrchidRoute(app);
+
+// const exampleData = [
+//     {
+//         name: 'Cattleya',
+//         description: 'The Queen of orchids',
+//     },
+//     {
+//         name: 'Dendrobium',
+//         description: 'Known for diversity and durability',
+//     },
+//     {
+//         name: 'Phalaenopsis',
+//         description: 'Often called the Moth Orchid',
+//     },
+//     {
+//         name: 'Oncidium',
+//         description: 'Recognized by its dancing lady-shaped flowers',
+//     },
+//     {
+//         name: 'Vanda',
+//         description: 'Known for large, vibrant flowers',
+//     },
+//     {
+//         name: 'Cymbidium',
+//         description: 'Popular for its use in corsages',
+//     },
+//     {
+//         name: 'Miltonia',
+//         description: 'Nicknamed the Pansy Orchid',
+//     },
+//     {
+//         name: 'Masdevallia',
+//         description: 'Distinctive for its showy and colorful blooms',
+//     },
+//     {
+//         name: 'Epidendrum',
+//         description: 'Characterized by its reed-like stems',
+//     },
+//     {
+//         name: 'Laelia',
+//         description: 'Often fragrant and elegant in appearance',
+//     },
+// ];
+// const url = 'mongodb://localhost:27017/shoppingFlower';
+// const connect = mongoose.connect(url, { family: 4 });
+
+// connect.then(() => {
+
+//     console.log('Connected correctly to server');
+
+//     Categories.insertMany(exampleData)
+//         .then((cate) => {
+//             console.log(cate);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+
+// });
 
 const port = process.env.PORT || 8888;
 
