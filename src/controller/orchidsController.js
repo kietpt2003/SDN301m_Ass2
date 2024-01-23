77f605ca6f16dca4020c4d2f6f53a184f756b168
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-23 23:35:25 +0700

    complete orchid controller with perfect rest API

--- src/controller/orchidsController.js
+++ src/controller/orchidsController.js
@@ -1,4 +1,4 @@
-import { createOrchid, getAllOrchids, updateOrc } from "../services/OrchidService"
+import { createOrchid, deleteOrchidById, getAllOrchids, updateOrc } from "../services/OrchidService"
 
 export const OrchidsPage = async (req, res) => {
     let arrOrchids = await getAllOrchids();
@@ -8,30 +8,52 @@ export const OrchidsPage = async (req, res) => {
 
 export const postOrchid = async (req, res) => {
     const data = await createOrchid(req.body);
-    console.log('check data: ', data);
     if (data.error) {
-        return res.status(200).render('OrchidHomePage.ejs', { arrOrchids: data.arrOrchids, error: data.error });
+        // return res.status(400).render('OrchidHomePage.ejs', { arrOrchids: data.arrOrchids, error: data.error });
+        return res.status(400).json({
+            status: 400,
+            message: "Invalid data",
+            error: data.error
+        });
     }
-    return res.status(200).render('OrchidHomePage.ejs', { arrOrchids: data.arrOrchids, isSuccess: data.isSuccess });
+    // return res.status(200).render('OrchidHomePage.ejs', { arrOrchids: data.arrOrchids, isSuccess: data.isSuccess });
+    return res.status(200).json({
+        status: 200,
+        data: data.data,
+        error: data.error
+    });
 }
 
 export const updateOrchid = async (req, res) => {
-    console.log('check data: ', req.body);
     const data = await updateOrc(req.body);
-    console.log('check: ', data);
     if (data.errorUpdate) {
-        return res.status(200).json({ arrCategories: data.arrCategories, errorUpdate: data.errorUpdate });
+        return res.status(400).json({
+            status: 400,
+            message: "Invalid data",
+            errorUpdate: data.errorUpdate
+        });
     }
-    return res.status(200).json({ arrCategories: data.arrCategories, isUpdate: data.isUpdate });
+    return res.status(200).json({
+        status: 200,
+        data: data.data,
+        isUpdate: data.isUpdate
+    });
 }
 
 export const deleteOrchid = async (req, res) => {
-    console.log('check data: ', req.params.id);
     const data = await deleteOrchidById(req.params.id);
     if (data.error) {
-        return res.status(200).json({ arrCategories: data.arrCategories, error: data.error });
+        return res.status(400).json({
+            status: 400,
+            message: "Invalid data",
+            error: data.error
+        });
         // return res.status(200).render('OrchidPage.ejs', { arrCategories: data.arrCategories, error: data.error });
     }
-    return res.status(200).json({ arrCategories: data.arrCategories, deleteSuccess: data.deleteSuccess });
+    return res.status(200).json({
+        status: 200,
+        data: data.data,
+        deleteSuccess: data.deleteSuccess
+    });
     // return res.render('OrchidPage.ejs', { arrCategories: data.arrCategories, deleteSuccess: data.deleteSuccess });
 }
