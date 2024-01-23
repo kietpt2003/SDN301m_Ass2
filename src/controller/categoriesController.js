3f51cc50a02e1d607592f40ba1832182bc6a1f07
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-23 20:52:18 +0700

    fix category controller and create update delete orchid controller

--- src/controller/categoriesController.js
+++ src/controller/categoriesController.js
@@ -9,7 +9,7 @@ export const postCategory = async (req, res) => {
     const data = await createCategory(req.body);
     console.log('check data: ', data);
     if (data.error) {
-        return res.status(400).render('CategoryPage.ejs', { arrCategories: data.arrCategories, error: data.error });
+        return res.status(200).render('CategoryPage.ejs', { arrCategories: data.arrCategories, error: data.error });
     }
     return res.status(200).render('CategoryPage.ejs', { arrCategories: data.arrCategories, isSuccess: data.isSuccess });
 }
@@ -19,7 +19,7 @@ export const updateCategory = async (req, res) => {
     const data = await updateCate(req.body);
     console.log('check: ', data);
     if (data.errorUpdate) {
-        return res.status(400).send({ arrCategories: data.arrCategories, errorUpdate: data.errorUpdate });
+        return res.status(200).json({ arrCategories: data.arrCategories, errorUpdate: data.errorUpdate });
     }
     return res.status(200).json({ arrCategories: data.arrCategories, isUpdate: data.isUpdate });
 }
@@ -28,7 +28,9 @@ export const deleteCategory = async (req, res) => {
     console.log('check data: ', req.params.id);
     const data = await deleteCategoryById(req.params.id);
     if (data.error) {
-        return res.status(400).render('CategoryPage.ejs', { arrCategories: data.arrCategories, error: data.error });
+        return res.status(200).json({ arrCategories: data.arrCategories, error: data.error });
+        // return res.status(200).render('CategoryPage.ejs', { arrCategories: data.arrCategories, error: data.error });
     }
-    return res.render('CategoryPage.ejs', { arrCategories: data.arrCategories, deleteSuccess: data.deleteSuccess });
+    return res.status(200).json({ arrCategories: data.arrCategories, deleteSuccess: data.deleteSuccess });
+    // return res.render('CategoryPage.ejs', { arrCategories: data.arrCategories, deleteSuccess: data.deleteSuccess });
 }
